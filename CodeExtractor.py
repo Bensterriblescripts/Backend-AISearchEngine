d807707b21f5728fc4a7aa12b486c68e4ef1a938
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2024-09-05 20:55:13 +1200

    sorting through variations of class/func/namespaces  in a codebase

--- CodeExtractor.py
+++ CodeExtractor.py
@@ -6,12 +6,12 @@ neo4j_uri = os.environ.get('NEO4J_URI')
 neo4j_user = os.environ.get('NEO4J_USER')
 neo4j_pass = os.environ.get('NEO4J_PASS')
 if (not neo4j_uri or not neo4j_user or not neo4j_pass):
-    print("Missing neo4j env variavles")
+    print("Missing neo4j env variables")
     exit()
 conn = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user,neo4j_pass))
 
 # Base folder with repository folders inside
-base_path = "C:\\Repositories\\Backend-AISearchEngine"
+base_path = "C:\\Repositories"
 print(f"Base path set to: {base_path}")
 
 # Ignore folders
@@ -88,6 +88,11 @@ def extractCodeByLine(codeFile, re_namespace, re_class, re_function):
     sub_function_brace = 0
     function_brace = 0
     class_brace = 0
+
+    codeNamespace[0] = None
+    class_name = None
+    fn_function = None
+    fn_sub_function = None
 
     # Please figure out a better way of doing this later...
     print(f"Entering file: {codeFile[1]}")
@@ -245,80 +250,100 @@ for repositoryFile in repositoryFiles:
         for classes in codeClasses:
             print(f"    Class: {classes[0]}\n")
             # print(f"    Class code: {codeWithinClass[classes[0]]}")
-        for function in codeFunctions:
-            print(f"    Function: {function[0]} | Start: {function[3]}, End: {function[4]}\n")
+        for functions in codeFunctions:
+            print(f"    Function: {functions[0]} | Start: {functions[3]}, End: {functions[4]}\n")
             # print(f"    Code:\n{codeWithinFunction[functions[0]]}")
         print("")
 
 
         # # Database Insertion
+        for function in codeFunctions:
 
-        # Test
-        function = codeFunctions[0]
-
-        #Add each function with repo, namespace, and class as well
-        addfunction = f'''
-        MERGE (r:Repository {{name: '{repositoryFile[0]}'}})
-        ON CREATE SET
-            r.added = datetime()
-        ON MATCH SET
-            r.modified = datetime()
-
-        MERGE (d:Document {{name: '{name}', type: '{extension}', path: '{repositoryFile[1]}', repository: '{repositoryFile[0]}'}})
-        ON CREATE SET
-            d.version = 1
-            d.added = datetime()
-        ON MATCH SET
-            d.version = COALESCE(d.version, 1) + 1
-            d.modified = datetime()
-
-        MERGE (n:Namespace {name: '{codeNamespace[0]}'})
+            #Add each function with repo, namespace, and class as well
+            addfunction = f'''
+            MERGE (r:Repository {{name: '{repositoryFile[0]}'}})
             ON CREATE SET
-            n.added = datetime()
+                r.added = datetime()
+            ON MATCH SET
+                r.modified = datetime()
 
-        MERGE (c:Class {name: '{classes[0]}', source: '{repositoryFile[2]}{repositoryFile[3]}'})
-        ON CREATE SET
-            c.version = 1
-            c.added = datetime()
-            c.content = $classContent
-        ON MATCH SET
-            c.version = COALESCE(c.version, 1) + 1
-            c.modified = datetime()
-            c.content = $classContent
-        
-        MERGE (f:Function {name: '{function[0]}'})
-        ON CREATE SET
-            f.version = 1
-            f.added = datetime()
-            f.content = $functionContent
-            f.linestart = {function[3]}
-            f.lineend = {function[4]}
-        ON MATCH SET
-            f.version = COALESCE(f.version, 1) + 1
-            f.modified = datetime()
-            f.content = $functionContent
-            f.linestart = {function[3]}
-            f.lineend = {function[4]}
+            MERGE (d:Document {{name: '{name}', type: '{extension}', path: '{repositoryFile[1]}', repository: '{repositoryFile[0]}'}})
+            ON CREATE SET
+                d.version = 1,
+                d.added = datetime()
+            ON MATCH SET
+                d.version = COALESCE(d.version, 1) + 1,
+                d.modified = datetime()
+            '''
+            if codeNamespace[0] is not None:
+                addfunction += f'''
+                MERGE (n:Namespace {{name: '{codeNamespace[0]}'}})
+                ON CREATE SET
+                    n.added = datetime()'''
 
-        MERGE (r)-[:CONTAINS]->(d)
+            if classes[0] is not None:
+                addfunction += f'''
+                MERGE (c:Class {{name: '{classes[0]}', source: '{repositoryFile[2]}{repositoryFile[3]}'}})
+                ON CREATE SET
+                    c.version = 1,
+                    c.added = datetime(),
+                    c.content = $classContent
+                ON MATCH SET
+                    c.version = COALESCE(c.version, 1) + 1,
+                    c.modified = datetime(),
+                    c.content = $classContent'''
+                
+            if function[0] is not None:
+                addfunction += f'''
+                MERGE (f:Function {{name: '{function[0]}'}})
+                ON CREATE SET
+                    f.version = 1,
+                    f.added = datetime(),
+                    f.content = $functionContent,
+                    f.linebegin = $linestart,
+                    f.lineend = $lineend
+                ON MATCH SET
+                    f.version = COALESCE(f.version, 1) + 1,
+                    f.modified = datetime(),
+                    f.content = $functionContent,
+                    f.linebegin = $linestart,
+                    f.lineend = $lineend'''
 
-        MERGE (d)-[:NAMESPACE]->(n)
-        MERGE (d)-[:CLASS]->(c)
-        MERGE (d)-[:FUNCTION]->(f)
+            if codeNamespace[0] is not None and classes[0] is not None and function[0] is not None:
+                addfunction += f'''
+                MERGE (r)-[:CONTAINS]->(d)
 
-        MERGE (n)-[:NAMESPACECLASS]->(c)
-        MERGE (n)-[:NAMESPACEFUNCTION]->(f)
+                MERGE (d)-[:NAMESPACE]->(n)
+                MERGE (d)-[:CLASS]->(c)
+                MERGE (d)-[:FUNCTION]->(f)
 
-        MERGE (c)-[:CLASSFUNCTION]->(f)
-        MERGE (c)-[:CLASSFUNCTION]->(f)'''
+                MERGE (n)-[:NAMESPACECLASS]->(c)
+                MERGE (n)-[:NAMESPACEFUNCTION]->(f)
+                
+                MERGE (c)-[:CLASSFUNCTION]->(f)'''
+            elif classes[0] is not None and function[0] is not None:
+                addfunction += f'''
+                    MERGE (r)-[:CONTAINS]->(d)
+                    MERGE (d)-[:CLASS]->(c)
+                    MERGE (d)-[:FUNCTION]->(f)
+                    MERGE (c)-[:CLASSFUNCTION]->(f)'''
+            elif function[0] is not None:
+                addfunction += '''
+                    MERGE (r)-[:CONTAINS]->(d)
+                    MERGE (d)-[:FUNCTION]->(f)'''
+            elif classes[0] is not None:
+                addfunction += '''
+                    MERGE (r)-[:CONTAINS]->(d)
+                    MERGE (d)-[:CLASS]->(c)'''
+                
 
-        parameter = {
-            "classContent": codeWithinClass[classes[0]],
-            "functionContent": codeWithinFunction[function[0]]
-        }
-        
-        results = neo4jquery(addfunction, parameter)
-        for record in results:
-            print(f"Name: {record['name']}, Age: {record['age']}")
-
-    # Add all by function - repository -> document -> namespace -> class -> function
+            parameter = {                                               # SQL injection rik make all var strings paramaterised
+                "classContent": codeWithinClass[classes[0]],
+                "functionContent": codeWithinFunction[function[0]],
+                "linestart": function[3],
+                "lineend": function[4]
+            }
+            
+            results = neo4jquery(addfunction, parameter)
+            for record in results:
+                print(record)
